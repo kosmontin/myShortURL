e5d9b87584fd270e5cec77dc32a66773eeaf86f6
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-09 17:19:35 +0300

    Step 6 of 12: add try-except block

--- shorten_url.py
+++ shorten_url.py
@@ -10,8 +10,9 @@ def shorten_link(token, url):
         'Content-Type': 'application/json'
     }
     data = {'long_url': url}
-    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=data).json()
-    return response['link']
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=data)
+    response.raise_for_status()
+    return 'Битлинк ' + response.json()['link']
 
 
 def main():
@@ -21,7 +22,10 @@ def main():
         print('Файл .env не найден в текущей папке или в нем отсутствует TOKEN')
         exit()
     url = input('Введите ссылку: ')
-    print(shorten_link(token, url))
+    try:
+        print(shorten_link(token, url))
+    except requests.exceptions.HTTPError as e:
+        print('Ошибка ввода: \n', e.args)
 
 
 if __name__ == '__main__':
